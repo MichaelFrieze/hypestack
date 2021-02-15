a706a5221ce3508ae18984c347a2b1faa9a73bb3
Michael Frieze <michaelfrieze@icloud.com>
2021-02-15 01:34:18 -0500

    server | installed dotenv and setup env variables for MikroORM to test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world!!!')
+import { MikroORM } from '@mikro-orm/core'
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: 'hypestack',
+    type: 'postgresql',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    debug: process.env.NODE_ENV !== 'production',
+  })
+}
+
+main()
