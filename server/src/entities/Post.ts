054a4d72dd83112307193dc0e5d5361e4f2ad67c
Michael Frieze <michaelfrieze@icloud.com>
2021-02-15 02:10:37 -0500

    server | server is now connecting to pg database, fixed dotenv problem

---
+++ server/src/entities/Post.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+
+/* 
+If I look in migrations/.....
+It infers the types so it used jsonb 
+so let's fix that using type property
+then deleting old migration
+*/
+
+@Entity()
+export class Post {
+  @PrimaryKey()
+  id!: number
+
+  @Property({ type: 'date' })
+  createdAt = new Date()
+
+  @Property({ type: 'date', onUpdate: () => new Date() })
+  updatedAt = new Date()
+
+  @Property({ type: 'text' })
+  title!: string
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,25 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [Post],
     dbName: 'hypestack',
-    type: 'postgresql',
-    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    type: 'postgresql',
     debug: !__prod__,
   })
+
+  const post = orm.em.create(Post, { title: 'my first post' })
+  await orm.em.persistAndFlush(post)
+  console.log('--------sql 2----------')
+  await orm.em.nativeInsert(Post, { title: 'my first post 2' })
 }
 
-main()
+main().catch((err) => {
+  console.error(err)
+})
